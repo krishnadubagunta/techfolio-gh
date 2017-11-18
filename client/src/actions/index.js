407881f85b32c7aabb50f4f9e6b95eae978e3485
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2017-11-17 20:31:03 -0500

    Token Reauth

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,13 @@ import { FETCH_USER, FETCH_GIT_VIEWER } from './types';
 import axios from 'axios';
 import { GraphQLClient } from 'graphql-request';
 import { fetchViewerQuery } from './graphQlRequests';
+import { GITHUB_KEY } from '../config';
 
 const GITHUB_GRAPH_QL_ENDPOINT = 'https://api.github.com/graphql';
 
 const client = new GraphQLClient(GITHUB_GRAPH_QL_ENDPOINT, {
   headers: {
-    Authorization: 'bearer 028cdfde5b775212642c9b470b9e363e6cc0a2ae'
+    Authorization: `bearer ${GITHUB_KEY}`
   }
 });
 
